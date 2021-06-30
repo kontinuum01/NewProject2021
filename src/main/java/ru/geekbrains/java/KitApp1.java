19bad6c9720cec5f583de9b96e772035b9b39c18
kontinuum01 <86378652+kontinuum01@users.noreply.github.com>
2021-06-30 23:28:56 +0300

    Update KitApp1.java

--- src/main/java/ru/geekbrains/java/KitApp1.java
+++ src/main/java/ru/geekbrains/java/KitApp1.java
@@ -5,7 +5,7 @@ public class KitApp1 {
     public static void main(String args[]) {
 
         massiveArray(8, 3);// Метод задающий целочисленный массив размером 8;
-                                    // С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
+                           // С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21;
     }
 
     static int[] massiveArray(int size, int step) {
